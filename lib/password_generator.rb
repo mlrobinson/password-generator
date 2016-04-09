cb8c36a054ab5cc2e4a24e0940c5160af89b1c01
Matthew Robinson <matthew.robinson@ensighten.com>
2016-04-09 16:24:50 -0700

    Initial files

---
+++ lib/password_generator.rb
@@ -0,0 +1,5 @@
+require "password_generator/version"
+
+module Password_Generator
+  # Your code goes here...
+end
---
+++ spec/password_generator_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Password_Generator do
+  it 'has a version number' do
+    expect(Password_Generator::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
