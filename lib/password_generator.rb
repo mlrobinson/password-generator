b95520e723a999f221ae5b545b9346f8b2e40fb4
Matthew Robinson <matthew.robinson@ensighten.com>
2016-04-09 18:56:33 -0700

    Remove rspec tests, modify random char selection functions

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -1,19 +1,6 @@
 require 'password_generator/version'
 require 'securerandom'
 
-# Built-in String class, monkey patch for insert_random
-class String
-  def insert_random(set)
-    random_char = SecureRandom.random_number(set.length)
-    random_pos = SecureRandom.random_number(length)
-    self[0..random_pos] + set[random_char] + self[random_pos + 1..length]
-  end
-
-  def insert_random!(set)
-    replace insert_random(set)
-  end
-end
-
 module PasswordGenerator
   # Password Generator class
   # This is setup to allow us to specify different password requirements for differnet systems
@@ -42,14 +29,18 @@ module PasswordGenerator
       raise 'You must enter at least one character set!' if @sets.empty?
     end
 
+    def get_random_char(str)
+      str[SecureRandom.random_number(str.length)]
+    end
+
     def generate(len)
       build_full_set if @full_set.empty?
       generated = ''
       (len - @sets.length).times do
-        generated += @full_set[SecureRandom.random_number(@full_set.length)]
+        generated = generated.insert(SecureRandom.random_number(generated.length), get_random_char(@full_set))
       end
       @sets.each do |set|
-        generated.insert_random!(set)
+        generated = generated.insert(SecureRandom.random_number(generated.length), get_random_char(set))
       end
       generated
     end
